61c54e5acf2a4d8f14f71fe1bbecddb112ed25d5
Maurício Azevedo <mauricio.neto@sou.inteli.edu.br>
2023-09-14 09:25:32 -0300

    feat: cria frontend para apresentação do modelo

---
+++ src/frontend/src/pages/HomePage/home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container, CompetenciaForm } from "./style";
+import NavBar from "../../components/NavBar";
+
+function HomePage() {
+  return <NavBar />;
+}
+
+export default HomePage;
