944238f5b18d0316c9b02606e804f7b7250a21d8
Maurício Azevedo <mauricio.neto@sou.inteli.edu.br>
2023-10-03 14:21:39 -0300

    fix: corrige bug da primeira requisição das vagas

--- src/frontend/src/services/getJobOpportunities.js
+++ src/frontend/src/services/getJobOpportunities.js
@@ -2,14 +2,46 @@ import axios from "axios";
 
 // Faça a requisição POST
 export async function GetJobOpportunities(data) {
-  return axios
-    .post("http://127.0.0.1:5000/calculate_compatibility", data)
-    .then(function (response) {
-      // Manipule a resposta bem-sucedida aqui
-      return response.data;
-    })
-    .catch(function (error) {
-      // Manipule qualquer erro que ocorra durante a requisição
-      console.error("Erro:", error);
-    });
+  if (data) {
+    return axios
+      .post("http://15.229.9.120:5000/calculate_compatibility", data)
+      .then(function (response) {
+        // Manipule a resposta bem-sucedida aqui
+        return response.data;
+      })
+      .catch(function (error) {
+        // Manipule qualquer erro que ocorra durante a requisição
+        console.error("Erro:", error);
+      });
+  } else {
+    const mockData = {
+      candidate: {
+        score_res: 46 / 90,
+        score_eng: 60 / 90,
+        score_int: 58 / 90,
+        score_cur: 48 / 90,
+        score_sin: 48 / 90,
+        score_dis: 48 / 90,
+        score_specialist: 0.4,
+        score_generalist: 0.6,
+        score_classic: 0.0,
+        score_order: 0.2222222222222222,
+        score_change: 0.4444444444444444,
+        score_tireless: 0.6666666666666666,
+        score_explorer: 0.3888888888888889,
+      },
+      limit: 10,
+    };
+
+    return axios
+      .post("http://127.0.0.1:5000/calculate_compatibility", mockData)
+      .then(function (response) {
+        // Manipule a resposta bem-sucedida aqui
+        return response.data;
+      })
+      .catch(function (error) {
+        // Manipule qualquer erro que ocorra durante a requisição
+        console.error("Erro:", error);
+      });
+  }
 }
